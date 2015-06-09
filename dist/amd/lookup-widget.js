cbcd87cdbb18699e6faf02f35d02608c80efca61
Tim Brown <tim@codelab.co.nz>
2015-06-10 09:50:11 +1200

    updated versioning and build

--- dist/amd/lookup-widget.js
+++ dist/amd/lookup-widget.js
@@ -5,7 +5,7 @@ define(['exports', 'aurelia-framework', 'jquery', 'select2'], function (exports,
 
   var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } };
 
-  var _createDecoratedClass = (function () { function defineProperties(target, descriptors, initializers) { for (var i = 0; i < descriptors.length; i++) { var descriptor = descriptors[i]; var decorators = descriptor.decorators; var key = descriptor.key; delete descriptor.key; delete descriptor.decorators; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor || descriptor.initializer) descriptor.writable = true; if (decorators) { for (var f = 0; f < decorators.length; f++) { var decorator = decorators[f]; if (typeof decorator === 'function') { descriptor = decorator(target, key, descriptor) || descriptor; } else { throw new TypeError('The decorator for method ' + descriptor.key + ' is of the invalid type ' + typeof decorator); } } if (initializers) initializers[key] = descriptor.initializer; } Object.defineProperty(target, key, descriptor); } } return function (Constructor, protoProps, staticProps, protoInitializers, staticInitializers) { if (protoProps) defineProperties(Constructor.prototype, protoProps, protoInitializers); if (staticProps) defineProperties(Constructor, staticProps, staticInitializers); return Constructor; }; })();
+  var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
   Object.defineProperty(exports, '__esModule', {
     value: true
@@ -16,30 +16,15 @@ define(['exports', 'aurelia-framework', 'jquery', 'select2'], function (exports,
   var _select22 = _interopRequire(_select2);
 
   var LookupWidget = (function () {
-    var _instanceInitializers = {};
-
     function LookupWidget(element) {
       _classCallCheck(this, _LookupWidget);
-
-      this.title = _instanceInitializers.title.call(this);
-      this.placeholder = _instanceInitializers.placeholder.call(this);
 
       this.element = element;
     }
 
     var _LookupWidget = LookupWidget;
 
-    _createDecoratedClass(_LookupWidget, [{
-      key: 'title',
-      decorators: [_aureliaFramework.bindable],
-      initializer: function () {},
-      enumerable: true
-    }, {
-      key: 'placeholder',
-      decorators: [_aureliaFramework.bindable],
-      initializer: function () {},
-      enumerable: true
-    }, {
+    _createClass(_LookupWidget, [{
       key: 'bind',
       value: function bind() {
         this.apply();
@@ -81,17 +66,19 @@ define(['exports', 'aurelia-framework', 'jquery', 'select2'], function (exports,
           _this.selectedItem = _$(_this.element).find('input').select2('val');
         });
       }
-    }], null, _instanceInitializers);
+    }]);
 
+    LookupWidget = _aureliaFramework.bindable('placeholder')(LookupWidget) || LookupWidget;
+    LookupWidget = _aureliaFramework.bindable('title')(LookupWidget) || LookupWidget;
     LookupWidget = _aureliaFramework.bindable({
       name: 'selectedItem',
       attribute: 'selected-item',
-      defaultBindingMode: _aureliaFramework.TWO_WAY
+      defaultBindingMode: _aureliaFramework.bindingMode.twoWay
     })(LookupWidget) || LookupWidget;
     LookupWidget = _aureliaFramework.bindable({
       name: 'controller',
       attribute: 'controller',
-      defaultBindingMode: _aureliaFramework.TWO_WAY
+      defaultBindingMode: _aureliaFramework.bindingMode.twoWay
     })(LookupWidget) || LookupWidget;
     LookupWidget = _aureliaFramework.customElement('lookup-widget')(LookupWidget) || LookupWidget;
     LookupWidget = _aureliaFramework.inject(Element)(LookupWidget) || LookupWidget;
