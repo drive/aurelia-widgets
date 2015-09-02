107dd4d10770002408a86a7b72b3276bc1474db3
Simon Fox <simon.fox@drivesoftware.co.nz>
2015-09-02 21:41:19 +1200

    add a simple combo box widget (html select with bootstrap styling)

---
+++ src/combo.js
@@ -0,0 +1,33 @@
+import {customElement, inject, bindable, bindingMode} from 'aurelia-framework';
+
+@customElement('combo')
+@bindable({
+  name:'options',
+  attribute:'options',
+  defaultBindingMode: bindingMode.oneTime
+})
+@bindable({
+  name:'selected',
+  attribute:'selected',
+  defaultBindingMode: bindingMode.twoWay
+})
+@inject(Element)
+export class Combo {
+  constructor(element) {
+    this.element = element;
+    this._boundChange = this._change.bind(this);
+  }
+
+  attached() {
+    this.combo = this.element.querySelector('select');
+    this.combo.addEventListener('change', this._boundChange);
+  }
+
+  detached() {
+    this.combo.removeEventListener('change', this._boundChange);
+  }
+
+  _change(change) {
+    this.selected = change.target.value;
+  }
+}
