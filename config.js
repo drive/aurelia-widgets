2cbb7f80f812dd12eef5ebefb6e0f37f0eceb402
Michael Malone <michael.malone@drivesoftware.co.nz>
2015-11-12 12:28:24 +1300

    Clutching at straws, trying to update dependencies

--- config.js
+++ config.js
@@ -16,25 +16,25 @@ System.config({
 
   map: {
     "aurelia-binding": "github:aurelia/binding@0.11.0",
-    "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.12.0",
+    "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.12.1",
     "aurelia-event-aggregator": "github:aurelia/event-aggregator@0.10.0",
-    "aurelia-templating": "github:aurelia/templating@0.17.0",
-    "babel": "npm:babel-core@5.8.25",
-    "babel-runtime": "npm:babel-runtime@5.8.25",
+    "aurelia-templating": "github:aurelia/templating@0.17.2",
+    "babel": "npm:babel-core@5.8.33",
+    "babel-runtime": "npm:babel-runtime@5.8.29",
     "core-js": "npm:core-js@1.2.6",
     "css": "github:systemjs/plugin-css@0.1.19",
     "devbridge/jQuery-Autocomplete": "github:devbridge/jQuery-Autocomplete@1.2.24",
     "jquery": "github:components/jquery@2.1.4",
     "select2": "github:select2/select2@3.5.4",
     "github:aurelia/binding@0.11.0": {
-      "aurelia-metadata": "github:aurelia/metadata@0.10.0",
+      "aurelia-metadata": "github:aurelia/metadata@0.10.1",
       "aurelia-pal": "github:aurelia/pal@0.3.0",
       "aurelia-task-queue": "github:aurelia/task-queue@0.9.0",
       "core-js": "npm:core-js@1.2.6"
     },
-    "github:aurelia/dependency-injection@0.12.0": {
+    "github:aurelia/dependency-injection@0.12.1": {
       "aurelia-logging": "github:aurelia/logging@0.9.0",
-      "aurelia-metadata": "github:aurelia/metadata@0.10.0",
+      "aurelia-metadata": "github:aurelia/metadata@0.10.1",
       "aurelia-pal": "github:aurelia/pal@0.3.0",
       "core-js": "npm:core-js@1.2.6"
     },
@@ -42,22 +42,22 @@ System.config({
       "aurelia-logging": "github:aurelia/logging@0.9.0"
     },
     "github:aurelia/loader@0.11.0": {
-      "aurelia-metadata": "github:aurelia/metadata@0.10.0",
+      "aurelia-metadata": "github:aurelia/metadata@0.10.1",
       "aurelia-path": "github:aurelia/path@0.11.0"
     },
-    "github:aurelia/metadata@0.10.0": {
+    "github:aurelia/metadata@0.10.1": {
       "aurelia-pal": "github:aurelia/pal@0.3.0",
       "core-js": "npm:core-js@1.2.6"
     },
     "github:aurelia/task-queue@0.9.0": {
       "aurelia-pal": "github:aurelia/pal@0.3.0"
     },
-    "github:aurelia/templating@0.17.0": {
+    "github:aurelia/templating@0.17.2": {
       "aurelia-binding": "github:aurelia/binding@0.11.0",
-      "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.12.0",
+      "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.12.1",
       "aurelia-loader": "github:aurelia/loader@0.11.0",
       "aurelia-logging": "github:aurelia/logging@0.9.0",
-      "aurelia-metadata": "github:aurelia/metadata@0.10.0",
+      "aurelia-metadata": "github:aurelia/metadata@0.10.1",
       "aurelia-pal": "github:aurelia/pal@0.3.0",
       "aurelia-path": "github:aurelia/path@0.11.0",
       "aurelia-task-queue": "github:aurelia/task-queue@0.9.0",
@@ -82,7 +82,7 @@ System.config({
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
-    "npm:babel-runtime@5.8.25": {
+    "npm:babel-runtime@5.8.29": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:core-js@1.2.6": {
