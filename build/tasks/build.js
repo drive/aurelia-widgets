5d0e26fe5914853c129b44522ca2cccf07f06aad
Michael Malone <michael.malone@drivesoftware.co.nz>
2016-03-30 16:21:24 +1300

    Upgrading to Babel6

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -4,9 +4,29 @@ var to5 = require('gulp-babel');
 var paths = require('../paths');
 var compilerOptions = require('../babel-options');
 var assign = Object.assign || require('object.assign');
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
+var changed = require('gulp-changed');
+var sourcemaps = require('gulp-sourcemaps');
+var through2 = require('through2');
+var tools = require('aurelia-tools');
+var concat = require('gulp-concat');
+var insert = require('gulp-insert');
 
-var moduleTypes = ['common', 'amd', 'system'];
-var buildTypes = ['build-es6'];
+var jsName = paths.packageName + '.js';
+var moduleTypes = ['commonjs', 'amd', 'system', 'es2015'];
+var buildTypes = [];
+
+gulp.task('build-system', function() {
+  return gulp.src(paths.source)
+    .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+    .pipe(changed(paths.output, {extension: '.js'}))
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(to5(assign({}, compilerOptions.system())))
+    .pipe(sourcemaps.write({includeContent: false, sourceRoot: '/src'}))
+    .pipe(gulp.dest(paths.output));
+});
+
 
 moduleTypes.forEach(function(mod) {
   gulp.task('build-styles-' + mod, function () {
@@ -21,32 +41,40 @@ moduleTypes.forEach(function(mod) {
 
   gulp.task('build-' + mod, ['build-html-' + mod,'build-styles-' + mod], function () {
     return gulp.src(paths.source)
-      .pipe(to5(assign({}, compilerOptions, {modules: mod})))
-      .pipe(gulp.dest(paths.output + mod));
+    .pipe(to5(assign({}, compilerOptions[mod]())))
+    .pipe(gulp.dest(paths.output + mod));
   });
   buildTypes.push('build-' + mod);
 });
 
-gulp.task('build-styles-es6', function () {
-  return gulp.src(paths.style)
-    .pipe(gulp.dest(paths.output + 'es6'));
-});
 
-gulp.task('build-html-es6', function () {
-  return gulp.src(paths.html)
-    .pipe(gulp.dest(paths.output + 'es6'));
-});
+gulp.task('build-index', function(){
+  var importsToAdd = [];
 
-gulp.task('build-es6', ['build-html-es6','build-styles-es6'], function () {
-  return gulp.src(paths.source)
-    .pipe(gulp.dest(paths.output + 'es6'));
+  return gulp.src([
+    paths.root + '*.js',
+    paths.root + '**/*.js',
+   '!' + paths.root + 'index.js'])
+    .pipe(through2.obj(function(file, enc, callback) {
+      file.contents = new Buffer(tools.extractImports(file.contents.toString("utf8"), importsToAdd));
+      this.push(file);
+      return callback();
+    }))
+    .pipe(concat(jsName))
+    .pipe(insert.transform(function(contents) {
+      return tools.createImportBlock(importsToAdd) + contents;
+    }))
+    .pipe(gulp.dest(paths.output));
 });
 
 
 gulp.task('build', function(callback) {
   return runSequence(
     'clean',
+    'build-index',
     buildTypes,
     callback
   );
 });
+
+
