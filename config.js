9c95257154f9033508496a4e6a51b02ba907e869
Simon Fox <simon.fox@drivesoftware.co.nz>
2017-02-27 21:40:02 +1300

    upgrade aurelia deps

--- config.js
+++ config.js
@@ -10,13 +10,13 @@ System.config({
 
   map: {
     "aurelia-animator-velocity": "npm:aurelia-animator-velocity@1.0.1",
-    "aurelia-binding": "npm:aurelia-binding@1.1.1",
+    "aurelia-binding": "npm:aurelia-binding@1.2.0",
     "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.3.0",
     "aurelia-event-aggregator": "npm:aurelia-event-aggregator@1.0.1",
     "aurelia-metadata": "npm:aurelia-metadata@1.0.3",
     "aurelia-pal-browser": "npm:aurelia-pal-browser@1.1.0",
     "aurelia-polyfills": "npm:aurelia-polyfills@1.2.0",
-    "aurelia-templating": "npm:aurelia-templating@1.2.0",
+    "aurelia-templating": "npm:aurelia-templating@1.3.0",
     "bootstrap": "github:twbs/bootstrap@3.3.7",
     "bootstrap-toggle": "npm:bootstrap-toggle@2.2.2",
     "css": "github:systemjs/plugin-css@0.1.26",
@@ -50,10 +50,10 @@ System.config({
     "npm:aurelia-animator-velocity@1.0.1": {
       "aurelia-metadata": "npm:aurelia-metadata@1.0.3",
       "aurelia-pal": "npm:aurelia-pal@1.3.0",
-      "aurelia-templating": "npm:aurelia-templating@1.2.0",
+      "aurelia-templating": "npm:aurelia-templating@1.3.0",
       "velocity-animate": "npm:velocity-animate@1.4.3"
     },
-    "npm:aurelia-binding@1.1.1": {
+    "npm:aurelia-binding@1.2.0": {
       "aurelia-logging": "npm:aurelia-logging@1.3.0",
       "aurelia-metadata": "npm:aurelia-metadata@1.0.3",
       "aurelia-pal": "npm:aurelia-pal@1.3.0",
@@ -82,8 +82,8 @@ System.config({
     "npm:aurelia-task-queue@1.2.0": {
       "aurelia-pal": "npm:aurelia-pal@1.3.0"
     },
-    "npm:aurelia-templating@1.2.0": {
-      "aurelia-binding": "npm:aurelia-binding@1.1.1",
+    "npm:aurelia-templating@1.3.0": {
+      "aurelia-binding": "npm:aurelia-binding@1.2.0",
       "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.3.0",
       "aurelia-loader": "npm:aurelia-loader@1.0.0",
       "aurelia-logging": "npm:aurelia-logging@1.3.0",
