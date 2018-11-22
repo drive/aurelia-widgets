3935c89ec342e62c76e155ad16968466cc796b60
Simon Fox <simon.fox@drivesoftware.co.nz>
2018-11-22 20:28:34 +1300

    chore(build): update build to latest skeleton setup

--- build/tasks/prepare-release.js
+++ build/tasks/prepare-release.js
@@ -6,30 +6,34 @@ var fs = require('fs');
 var bump = require('gulp-bump');
 var args = require('../args');
 
-gulp.task('bump-version', function(){
-  return gulp.src(['./package.json', './bower.json'])
-    .pipe(bump({type:args.bump })) //major|minor|patch|prerelease
+// utilizes the bump plugin to bump the
+// semver for the repo
+gulp.task('bump-version', function () {
+  return gulp.src(['./package.json'])
+    .pipe(bump({ type: args.bump })) //major|minor|patch|prerelease
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('changelog', function(callback) {
+// generates the CHANGELOG.md file based on commit
+// from git commit messages
+gulp.task('changelog', function (callback) {
   var pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
 
   return changelog({
     repository: pkg.repository.url,
     version: pkg.version,
     file: paths.doc + '/CHANGELOG.md'
-  }, function(err, log) {
+  }, function (err, log) {
     fs.writeFileSync(paths.doc + '/CHANGELOG.md', log);
   });
 });
 
-gulp.task('prepare-release', function(callback){
+// calls the listed sequence of tasks in order
+gulp.task('prepare-release', function (callback) {
   return runSequence(
     'build',
     'lint',
     'bump-version',
-    'doc',
     'changelog',
     callback
   );
