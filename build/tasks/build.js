a9f66e03ee5831d1c8222f58e880dac7e2aba840
Simon Fox <simon.fox@drivesoftware.co.nz>
2018-11-22 20:28:34 +1300

    chore(build): update build to latest skeleton setup

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -1,80 +1,55 @@
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
+var changed = require('gulp-changed');
+var plumber = require('gulp-plumber');
 var to5 = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
 var paths = require('../paths');
 var compilerOptions = require('../babel-options');
 var assign = Object.assign || require('object.assign');
-var plumber = require('gulp-plumber');
 var notify = require('gulp-notify');
-var changed = require('gulp-changed');
-var sourcemaps = require('gulp-sourcemaps');
-var through2 = require('through2');
-var tools = require('aurelia-tools');
-var concat = require('gulp-concat');
-var insert = require('gulp-insert');
+var browserSync = require('browser-sync');
+var htmlmin = require('gulp-htmlmin');
 
-var jsName = paths.packageName + '.js';
-var moduleTypes = ['commonjs', 'amd', 'system', 'es2015'];
-var buildTypes = [];
-
-gulp.task('build-system', function() {
+// transpiles changed es6 files to AMD format
+// the plumber() call prevents 'pipe breaking' caused
+// by errors from other gulp plugins
+// https://www.npmjs.com/package/gulp-plumber
+gulp.task('build-system', function () {
   return gulp.src(paths.source)
-    .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
-    .pipe(changed(paths.output, {extension: '.js'}))
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(to5(assign({}, compilerOptions.system())))
-    .pipe(sourcemaps.write({includeContent: false, sourceRoot: '/src'}))
+    .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
+    .pipe(changed(paths.output, { extension: '.js' }))
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(to5(assign({}, compilerOptions('amd'))))
+    .pipe(sourcemaps.write('.', { includeContent: false, sourceRoot: '/src' }))
     .pipe(gulp.dest(paths.output));
 });
 
-
-moduleTypes.forEach(function(mod) {
-  gulp.task('build-styles-' + mod, function () {
-    return gulp.src([paths.style, paths.publishedSass])
-      .pipe(gulp.dest(paths.output + mod));
-  });
-
-  gulp.task('build-html-' + mod, function () {
-    return gulp.src(paths.html)
-      .pipe(gulp.dest(paths.output + mod));
-  });
-
-  gulp.task('build-' + mod, ['build-html-' + mod,'build-styles-' + mod], function () {
-    return gulp.src(paths.source)
-    .pipe(to5(assign({}, compilerOptions[mod]())))
-    .pipe(gulp.dest(paths.output + mod));
-  });
-  buildTypes.push('build-' + mod);
-});
-
-
-gulp.task('build-index', function(){
-  var importsToAdd = [];
-
-  return gulp.src([
-    paths.root + '*.js',
-    paths.root + '**/*.js',
-   '!' + paths.root + 'index.js'])
-    .pipe(through2.obj(function(file, enc, callback) {
-      file.contents = new Buffer(tools.extractImports(file.contents.toString("utf8"), importsToAdd));
-      this.push(file);
-      return callback();
-    }))
-    .pipe(concat(jsName))
-    .pipe(insert.transform(function(contents) {
-      return tools.createImportBlock(importsToAdd) + contents;
-    }))
+// copies changed html files to the output directory
+gulp.task('build-html', function () {
+  return gulp.src(paths.html)
+    .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
+    .pipe(changed(paths.output, { extension: '.html' }))
+    .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest(paths.output));
 });
 
+// copies changed css files to the output directory
+gulp.task('build-css', function () {
+  return gulp.src(paths.css)
+    .pipe(changed(paths.output, { extension: '.css' }))
+    .pipe(gulp.dest(paths.output))
+    .pipe(browserSync.stream());
+});
 
-gulp.task('build', function(callback) {
+// this task calls the clean task (located
+// in ./clean.js), then runs the build-system
+// and build-html tasks in parallel
+// https://www.npmjs.com/package/gulp-run-sequence
+gulp.task('build', function (callback) {
   return runSequence(
     'clean',
-    'build-index',
-    buildTypes,
+    ['build-system', 'build-html', 'build-css'],
     callback
   );
 });
-
-
--- build/tasks/test.js
+++ build/tasks/test.js
@@ -1,27 +1,38 @@
 var gulp = require('gulp');
-var karma = require('karma');
+var Karma = require('karma').Server;
 
 /**
  * Run test once and exit
  */
 gulp.task('test', function (done) {
-    var server = new karma.Server({
-        configFile: __dirname + '/../../karma.conf.js',
-        singleRun: true
-    }, function(e) {
-        done();
-    });
-    server.start();
+  new Karma({
+    configFile: __dirname + '/../../karma.conf.js',
+    singleRun: true
+  }, done).start();
 });
 
 /**
  * Watch for file changes and re-run tests on each change
  */
 gulp.task('tdd', function (done) {
-    var server = new karma.Server({
-        configFile: __dirname + '/../../karma.conf.js'
-    }, function(e) {
-        done();
-    });
-    server.start();
+  new Karma({
+    configFile: __dirname + '/../../karma.conf.js'
+  }, done).start();
+});
+
+/**
+ * Run test once with code coverage and exit
+ */
+gulp.task('cover', function (done) {
+  new Karma({
+    configFile: __dirname + '/../../karma.conf.js',
+    singleRun: true,
+    reporters: ['coverage'],
+    coverageReporter: {
+      reporters: [
+        { type: 'html', dir: 'coverage' },
+        { type: 'text' }
+      ]
+    }
+  }, done).start();
 });
